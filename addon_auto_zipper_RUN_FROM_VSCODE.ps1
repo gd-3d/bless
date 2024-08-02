5e34c7453a0f436962f9868e2d89e19d06d8d1a3
yankscally <yankscally@gmail.com>
2024-08-02 16:37:08 +0100

    made some mess when cleaning up

--- addon_auto_zipper_RUN_FROM_VSCODE.ps1
+++ addon_auto_zipper_RUN_FROM_VSCODE.ps1
@@ -1,17 +1,19 @@
 $parent_directory = Split-Path -Path (Get-Location) -Leaf -Resolve
 $target_subfolder = Split-Path -Path ".\$parent_directory" -Leaf -Resolve
+$addon_name = "bless"
+
 
 if ( get-childitem -Include __pycache__ -Recurse -force )
 {
     get-childitem -Include __pycache__ -Recurse -force | Remove-Item -Force -Recurse
 }
-if ( Test-Path ".\$target_subfolder\addon" )
+if ( Test-Path ".\$target_subfolder\$name" )
 {
-    Remove-Item  -Recurse -Path ".\$target_subfolder\addon"
+    Remove-Item  -Recurse -Path ".\$target_subfolder\$name"
 }
 if ( Test-Path ".\$target_subfolder")
 {
-    Compress-Archive -Force -Path .\$target_subfolder -Destination ".\$target_subfolder.zip"
+    Compress-Archive -Force -Path .\$target_subfolder -Destination ".\$addon_name.zip"
 } 
 
 # write-host "Press any key to continue..."
