2113ec92dc5fbd3ecf9b6780a7559b30fc30ed39
yankscally <yankscally@gmail.com>
2024-11-05 00:42:59 +0000

    working exports

--- bless/gltf/gltf_export.py
+++ bless/gltf/gltf_export.py
@@ -2,7 +2,7 @@ import copy
 import bpy
 
 #### constants
-LIGHT_FACTOR = 680
+gltf_light_factor = 680
 
 
 
@@ -31,7 +31,7 @@ bless_extensions =     [
 
 
 
-#TODO find a way to "install" these automagically from outside the addon.
+#TODO find a way to "install" these automagically from outside the file.
 # glTF extensions library / package manager...
 user_extensions = []
 
@@ -39,19 +39,6 @@ user_extensions = []
 
 node_tree = {}
 materials = {}
-
-
-collision_tree = []
-
-trimesh_collisions = []
-convex_collisions = []
-custom_collisions = []
-
-# each node as a dict with each flag.
-# locked (selectable)
-# hidden (in)
-# exclude from render...
-node_flags = []
 
 ###### EXPORT #######
 
@@ -85,69 +72,18 @@ class bless_glTF2Extension:
         if hasattr(blender_object, "type"):
             bless_print(f"Object type: [{blender_object.type}]")
             node_tree[blender_object.name] = {}
-            
-
-            
-            
-            
-            # Dictionary to store the node flags for each Blender object
-            node_flags = {}
-            
-            # Collect the lock, hidden, and exclude properties for each Blender object
-            locked = blender_object.hide_select  # Determines if the object is locked
-            hidden = blender_object.hide_get()  # Determines if the object is hidden
-            exclude = blender_object.hide_render  # Determines if the object is excluded from rendering
-
-            # Add these flags to a dictionary for the current node
-            node_flags["locked"] = locked
-            node_flags["hidden"] = hidden
-            node_flags["exclude"] = exclude
-
-            # Create the final dictionary with the object name as the key and node flags as the value
-            node_tree[blender_object.name] = node_flags
-
-            # Print the node flags dictionary for debugging
-            print(node_flags)
-            
-
-
             if blender_object.type == "MESH":
-        
                 node_tree[blender_object.name]["type"] = "mesh"
-                
-                print("node is: ",node_tree[blender_object.name], "and type: ", node_tree[blender_object.name]["type"])
-            
+
             elif blender_object.type == "LIGHT":
                 node_tree[blender_object.name]["type"] = "light"
-                print("node is: ",node_tree[blender_object.name], "and type: ", node_tree[blender_object.name]["type"])
 
             elif blender_object.type == "CAMERA":
                 node_tree[blender_object.name]["type"] = "camera"
-                print("node is: ",node_tree[blender_object.name], "and type: ", node_tree[blender_object.name]["type"])
         else:
             # its a collection.
             node_tree[blender_object.name] = {}
             node_tree[blender_object.name]["type"] = "collection"
-            print("collection is: ", blender_object.name)
-
-            collection = blender_object
-            
-            if blender_object.get("collision"):
-                if blender_object["collision"] == "":
-                    # exact type value or no shape is given.
-                    pass
-
-                # otherwise, sort the tagged objects into their piles for later.
-
-                if blender_object["collision"] == "trimesh":
-                    trimesh_collisions.append(blender_object.name )
-                elif blender_object["collision"] == "convex":
-                    convex_collisions.append(blender_object.name )
-                
-                ## TODO, implement compound / custom collision setups.
-                # elif blender_object["collision"] == "custom":
-                #     custom_collisions.append(blender_object)   
-          
 
     
     
@@ -163,21 +99,22 @@ class bless_glTF2Extension:
         # First pass: Create shapes and body nodes
         for i, node in enumerate(gltf_plan.nodes):
             if node.name in node_tree:
-                if node_tree[node.name]["type"] == "mesh":
-                    # Create shape
-                    shape = build_shape_dictionary("convex", node.mesh)
-                    shapes.append(shape)
-                    
-                    # Create body node
-                    body = copy.deepcopy(node)
-                    body.name = f"{node.name}_body"
-                    body.extensions["OMI_physics_body"] = build_body_dictionary("static", shape_index=len(shapes) - 1)
-                    node.translation = None
-                    node.rotation = None
-                    node.scale = None
-                    
-                    bodies.append(body)
-                    node_map[i] = len(gltf_plan.nodes) + len(bodies) - 1  # Map original index to new body index
+                if "type" in node_tree[node.name]:
+                    if node_tree[node.name]["type"] == "mesh":
+                        # Create shape
+                        shape = build_shape_dictionary("trimesh", node.mesh)
+                        shapes.append(shape)
+                        
+                        # Create body node
+                        body = copy.deepcopy(node)
+                        body.name = f"{node.name}_Body_"
+                        body.extensions["OMI_physics_body"] = build_body_dictionary("static", shape_index=len(shapes) - 1)
+                        node.translation = None
+                        node.rotation = None
+                        node.scale = None
+                        
+                        bodies.append(body)
+                        node_map[i] = len(gltf_plan.nodes) + len(bodies) - 1  # Map original index to new body index
 
         # Second pass: Update parent-child relationships
         for i, node in enumerate(gltf_plan.nodes):
@@ -217,7 +154,11 @@ class bless_glTF2Extension:
             required=False
         )
 
+
+        
+
         bless_print("Gather extensions finished", header=True)
+
 
 
 
@@ -253,37 +194,3 @@ def build_shape_dictionary(shape_type, mesh_index=-1, size=None, radius=None, he
             shape_data[shape_type]["height"] = height or 2.0
 
     return shape_data
-
-
-        #TODO FIXME HACK MOVE TO GODOT !!!!!!
-
-        ### gltf fixes...
-        # these are issues/bugs that a little elbow grease and a few lines of code can solve :]
-
-        # ## Lights
-        # # lights are too bright, inconsistent and light units are different than blenders.
-        # if "KHR_lights_punctual" in gltf_plan.extensions:
-        #     for light_data in gltf_plan.extensions["KHR_lights_punctual"]["lights"]:
-        #         blender_intensity = light_data["intensity"]
-                
-        #         # TODO, convert light intensity properly.
-        #         if blender_intensity > 100.0:
-        #             light_data["intensity"] = blender_intensity / 680 # light factor for godot
-                
-        #         # TODO, fix distances for point lights
-
-
-        
-        # ## Materials
-        # # KHR texture transform is inconsistent / scale is incorrect
-        # # https://github.com/KhronosGroup/glTF/blob/main/extensions/2.0/Khronos/KHR_texture_transform
-        # for material in gltf_plan.materials:
-        #     print(material.name)
-        #     extensions = material.pbr_metallic_roughness.extensions # FIXME - why is this empty?
-        #     for ext in extensions: 
-        #         print(ext)
-
-        #TODO FIXME HACK MOVE TO GODOT !!!!!!
-
-
-
