eb1a9c0e3a2cd233bb3b079e23822d6695fd0e34
Valery-AA <valeryatart@gmail.com>
2025-01-30 01:54:53 +0100

    object data UI skeleton

--- user_nterface/bless_object_datasheet.py
+++ user_nterface/bless_object_datasheet.py
@@ -1,10 +1,13 @@
 import bpy
 
+from ..modules.Alx_Module_Manager_Utils import define_dependency
 
+
+@define_dependency("BLESS_View3d_ObjectData")
 class BLESS_View3d_ObjectDataSheet(bpy.types.Panel):
     """"""
 
-    bl_label = ""
+    bl_label = "BLESS Object Data"
     bl_idname = "BLESS_PT_view3d_object_data_sheet"
 
     bl_parent_id = "bless_PT_view3d_object_data"
@@ -18,4 +21,6 @@ class BLESS_View3d_ObjectDataSheet(bpy.types.Panel):
 
     def draw(self, context: bpy.types.Context):
         layout = self.layout
-        layout.box().label(text="THIS IS TEST")
+        if (context.object is not None) and (hasattr(context.object, "bless_object_collision_settings")):
+            object_collision = context.object.bless_object_collision_settings
+            layout.prop(object_collision, "collision_types")
